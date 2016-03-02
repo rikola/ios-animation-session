37e50b56cb309b31d97f9344090e6636158ef991
Eric <rikola2@gmail.com>
2016-03-01 23:18:12 -0600

    Completed car animations

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -10,16 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var carImage: UIImageView!
+	
+	@IBOutlet weak var upButton: UIButton!
+	@IBOutlet weak var downButton: UIButton!
+	@IBOutlet weak var leftButton: UIButton!
+	@IBOutlet weak var rightButton: UIButton!
+	
+	var isVertical = true
+	
+	// Put all your initial view configuration logic in here
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		carImage.transform = CGAffineTransformMakeRotation(CGFloat(M_PI_2))
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	
+	@IBAction func upButtonDidPress(sender: UIButton) {
+		UIView.animateWithDuration(1.0, animations: {
+			self.carImage.center.y -= 50.0
+		})
 	}
-
-
+	
+	@IBAction func downButtonDidPress(sender: UIButton) {
+		UIView.animateWithDuration(1.0, animations: {
+			self.carImage.center.y += 50.0
+		})
+	}
+	
+	@IBAction func rightButtonDidPress(sender: UIButton) {
+		UIView.animateWithDuration(1.0, animations: {
+			self.carImage.center.x += 50.0
+		})
+	}
+	
+	@IBAction func leftButtonDidPress(sender: UIButton) {
+		UIView.animateWithDuration(1.0, animations: {
+			self.carImage.center.x -= 50.0
+		})
+	}
+	
+	@IBAction func turnLeftDidPress(sender: UIButton) {
+		toggleVertical()
+		UIView.animateWithDuration(1.0, animations: {
+			let currentTransform = self.carImage.transform
+			self.carImage.transform = CGAffineTransformRotate(currentTransform, CGFloat(-M_PI_2))
+		})
+	}
+	
+	@IBAction func turnRightDidPress(sender: UIButton) {
+		toggleVertical()
+		UIView.animateWithDuration(1.0, animations: {
+			let currentTransform = self.carImage.transform
+			self.carImage.transform = CGAffineTransformRotate(currentTransform, CGFloat(M_PI_2))
+		})
+	}
+	
+	
+	func toggleVertical() {
+		isVertical = !isVertical
+		upButton.enabled = !upButton.enabled
+		downButton.enabled = !downButton.enabled
+		leftButton.enabled = !leftButton.enabled
+		rightButton.enabled = !rightButton.enabled
+	}
+	
+	
 }
 
